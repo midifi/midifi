286dd10f350fecf02ee8561059a6bf7886d290d9
Ryan Gonzalez <rymg19@gmail.com>
2015-12-27 18:44:52 -0600

    More building

--- fbuildroot.py
+++ fbuildroot.py
@@ -2,6 +2,7 @@ from fbuild.builders.cxx import guess_static, guess_shared
 from fbuild.builders.c.msvc import Builder as MsvcBuilder
 from fbuild.builders.c.gcc import Builder as GccBuilder
 from fbuild.builders.pkg_config import PkgConfig
+from fbuild.builders.c import Library
 from fbuild.builders import find_program
 from fbuild.record import Record
 from fbuild.path import Path
@@ -84,6 +85,46 @@ class Felix(fbuild.db.PersistentObject):
                 else:
                     self.ctx.logger.passed()
 
+    def uncached_compile(self, dst, src, cxx, includes=[], pkgconfig_paths=[],
+                         libpaths=[], libs=[], cflags=[]):
+        dst = Path(dst).addroot(self.ctx.buildroot)
+        dst = dst.replaceext(cxx.exe_linker.suffix)
+
+        new_libs = []
+
+        def process_library(lib):
+            if isinstance(lib, Library):
+                for sublib in lib.libs:
+                    process_library(sublib)
+
+                new_libs.extend(lib.external_libs)
+                libpaths.append(lib.dirname())
+                slib = str(lib.basename().replaceext(''))
+                if slib.startswith('lib') and lib.splitext()[1] == '.a':
+                    slib = slib[3:]
+                new_libs.append(slib)
+                libpaths.extend(lib.libpaths)
+
+        for lib in libs:
+            process_library(lib)
+
+        cmd = [self.flx, '-c', '--static']
+        cmd.extend('--pkgconfig-path+=' + path for path in pkgconfig_paths)
+        cmd.extend('-I' + include for include in includes)
+        cmd.extend('-L' + path for path in libpaths)
+        cmd.extend('-l' + lib for lib in new_libs)
+        cmd.extend('--cflags=' + flag for flag in cflags)
+        cmd.extend(('-o', dst))
+        cmd.append(src)
+
+        self.ctx.execute(cmd, 'flx', '%s -> %s' % (src, dst), color='link')
+        return dst
+
+    @fbuild.db.cachemethod
+    def compile(self, dst, src: fbuild.db.SRC, others: fbuild.db.SRCS, *args,
+        **kw) -> fbuild.db.DST:
+        return self.uncached_compile(dst, src, *args, **kw)
+
     def uncached_run(self, path, *args, **kw):
         return self.ctx.execute([self.flx, path], *args, **kw)
 
@@ -151,6 +192,16 @@ def build_midifile(ctx, rec):
     return rec.static.build_lib('midifile',
         Path.glob('midifile/src-library/*.cpp'), includes=['midifile/include'])
 
+def build_midifi(ctx, rec, midifile):
+    all_sources = []
+    for pat in 'midifi/*.flx', 'sfml/sfml/*.flx', 'midifile.flx/midifile.flx':
+        all_sources.extend(Path.glob(pat))
+
+    rec.felix.compile('midifi', 'midifi.flx', all_sources, rec.static,
+                      includes=['sfml', 'midifile', 'midifile.flx'],
+                      pkgconfig_paths=['build/config'], libs=[midifile])
+
 def build(ctx):
     rec = configure(ctx)
     midifile = build_midifile(ctx, rec)
+    build_midifi(ctx, rec, midifile)
